1021d4c9d82d2d8abc277a5e8835e6616b5cc07c
byonduniverse <masterjames318@gmail.com>
2022-10-06 00:04:10 +0900

    changed config

--- src/db/config.ts
+++ src/db/config.ts
@@ -2,6 +2,6 @@ export const dbConfig = {
   mongoose: {
     uri:
       process.env.REACT_APP_DATABASE_URL ||
-      'mongodb+srv://root:akubO3WozJNkVaEb@chat.o7uxt5z.mongodb.net/chatroom',
+      'mongodb://127.0.0.1:27017/chatroom',
   },
 }
